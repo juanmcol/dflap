96f8c9bdd6351cdbf694e280c48193dddc928edc
juanmcol <jmcolina1996@gmail.com>
2026-06-23 21:00:20 -0500

    Fix router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -5,18 +5,21 @@ import ContactPage from '../pages/ContactPage.jsx';
 import AboutPage from '../pages/AboutPage.jsx';
 import NotFoundPage from '../pages/NotFoundPage.jsx';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <AppPage/>,
-    errorElement: <NotFoundPage/>
-  },
-  {
-    path: '/contact',
-    element: <ContactPage/>
-  },
-  {
-    path: '/about',
-    element: <AboutPage/>
-  }
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <AppPage/>,
+      errorElement: <NotFoundPage/>
+    },
+    {
+      path: '/contact',
+      element: <ContactPage/>
+    },
+    {
+      path: '/about',
+      element: <AboutPage/>
+    }
+  ],
+  { basename: '/dflap' } 
+);
